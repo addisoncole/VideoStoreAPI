d94295da091ea28c541fbe84ebecbda7f33fcf12
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-05 20:54:36 -0800

    Add error assertions to movie validation tests

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -6,21 +6,25 @@ describe Movie do
   let(:no_inventory) {movie.inventory = nil}
 
   describe "validations" do
+    it "requires title and inventory to be valid" do
+      expect(movie).must_be :valid?
+
+      invalid_movie = Movie.new
+      expect(invalid_movie).wont_be :valid?
+      expect(invalid_movie.errors.messages).must_include :inventory, :title
+    end
+
     it "is invalid without title" do
       no_title
       expect(movie).wont_be :valid?
+      expect(movie.errors.messages).must_include :title
     end
 
     it "is invalid without inventory" do
       no_inventory
       expect(movie).wont_be :valid?
-    end
-
-    it "requires title and inventory to be valid" do
-      expect(movie).must_be :valid?
-      expect(Movie.new).wont_be :valid?
+      expect(movie.errors.messages).must_include :inventory
     end
   end
-
 
 end
