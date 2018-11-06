81b881e1f27b4325e4ed697dd502ba716da0db73
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-05 21:01:22 -0800

    Refactor test data

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -2,26 +2,27 @@ require "test_helper"
 
 describe Movie do
   let(:movie) { movies(:one) }
-  let(:no_title) {movie.title = nil}
-  let(:no_inventory) {movie.inventory = nil}
 
   describe "validations" do
     it "requires title and inventory to be valid" do
       expect(movie).must_be :valid?
 
       invalid_movie = Movie.new
+      
       expect(invalid_movie).wont_be :valid?
       expect(invalid_movie.errors.messages).must_include :inventory, :title
     end
 
     it "is invalid without title" do
-      no_title
+      movie.title = nil
+
       expect(movie).wont_be :valid?
       expect(movie.errors.messages).must_include :title
     end
 
     it "is invalid without inventory" do
-      no_inventory
+      movie.inventory = nil
+
       expect(movie).wont_be :valid?
       expect(movie.errors.messages).must_include :inventory
     end
