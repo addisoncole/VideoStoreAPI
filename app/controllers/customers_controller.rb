6aaf8c38464185e0e1841e113d3fd01c9a148f8f
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-06 12:41:33 -0800

    Add CustomersController#index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,16 @@
 class CustomersController < ApplicationController
+  def index
+    customers = Customer.all
+    render json: jsonify(customers)
+  end
 end
+
+private
+
+  def customer_params
+    params.require(:customer).permit(:name, :registered_at, :address, :city, :state, :postal_code, :phone)
+  end
+
+  def jsonify(customer_data)
+    return customer_data.as_json( only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone] )
+  end
