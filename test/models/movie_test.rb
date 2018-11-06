f6394893f69c11a21aa7ef055dea945ff31737a8
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-05 20:34:53 -0800

    Movie model validation tests

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,26 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  let(:movie) { movies(:one) }
+  let(:no_title) {movie.title = nil}
+  let(:no_inventory) {movie.inventory = nil}
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  describe "validations" do
+    it "is invalid without title" do
+      no_title
+      expect(movie).wont_be :valid?
+    end
+
+    it "is invalid without inventory" do
+      no_inventory
+      expect(movie).wont_be :valid?
+    end
+
+    it "requires title and inventory to be valid" do
+      expect(movie).must_be :valid?
+      expect(Movie.new).wont_be :valid?
+    end
   end
+
+
 end
