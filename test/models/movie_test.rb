13b28129af12710e9863ff1452b8a9f4313464b6
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-05 23:28:03 -0800

    Tests for rentals relation

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -8,7 +8,7 @@ describe Movie do
       expect(movie).must_be :valid?
 
       invalid_movie = Movie.new
-      
+
       expect(invalid_movie).wont_be :valid?
       expect(invalid_movie.errors.messages).must_include :inventory, :title
     end
@@ -25,6 +25,22 @@ describe Movie do
 
       expect(movie).wont_be :valid?
       expect(movie.errors.messages).must_include :inventory
+    end
+  end
+
+  describe "relations" do
+    it "has a list of rentals" do
+      movie.must_respond_to :rentals
+      movie.rentals.each do |rental|
+        rental.must_be_kind_of Rental
+      end
+    end
+
+    it "returns empty array if no rentals" do
+      rental = rentals(:one)
+
+      expect(movie.rentals.count).must_equal 0
+      expect(movie.rentals).must_equal []
     end
   end
 
