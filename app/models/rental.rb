a8263a0846d750d2c104254c06c69c2c7a1ec943
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-07 11:08:24 -0800

    .set_rental_dates hod for Rentals#checkout

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,11 @@
 class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :movie
+
+  def set_rental_dates
+    date = DateTime.now
+    self.checkout_date = date
+    date += 7
+    self.due_date = date
+  end
 end
