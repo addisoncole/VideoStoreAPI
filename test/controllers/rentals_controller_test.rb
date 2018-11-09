dc6f6e13b329a5fab533179258fc28d413e37d29
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-09 07:51:11 -0800

    Tests for checkin

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 describe RentalsController do
+  let(:rental_data) {
+     { "movie_id": movies(:goonies).id,
+       "customer_id": customers(:sontag).id }
+     }
+  let(:bad_rental_data) {
+    { "movie_id": 0, "customer_id": 0 }
+    }
 
   def check_response(expected_type:, expected_status: :success)
     must_respond_with expected_status
@@ -14,10 +21,7 @@ describe RentalsController do
   end
 
   describe "checkout" do
-    let(:rental_data) {
-       { "movie_id": movies(:goonies).id,
-         "customer_id": customers(:sontag).id }
-       }
+
     it "creates new rental with valid request data" do
 
 
@@ -34,10 +38,7 @@ describe RentalsController do
       expect(rental.customer_id).must_equal rental_data[:customer_id]
     end
     it "does not create new rental with invalid request data" do
-      bad_rental_data = {
-        "movie_id": 0,
-        "customer_id": 0
-      }
+
       post checkout_path, params: bad_rental_data, as: :json
       body = check_response(expected_type: Hash, expected_status: :bad_request)
       expect(body).must_include "errors"
@@ -54,8 +55,28 @@ describe RentalsController do
       body = check_response(expected_type: Hash, expected_status: :bad_request)
       expect(body).must_include "errors"
       expect(body["errors"]).must_include "movie_inventory"
+    end
+  end
 
+  describe "checkin" do
+    it "changes rental checked_in status to false with valid request data" do
+      post checkout_path, params: rental_data, as: :json
+      body = JSON.parse(response.body)
+      rental = Rental.find(body["id"].to_i)
+      expect(rental.checked_out?).must_equal true
 
+      post checkin_path, params: rental_data, as: :json
+      body = JSON.parse(response.body)
+      rental = Rental.find(body["id"].to_i)
+      expect(rental.checked_out?).must_equal false
+      must_respond_with :success
+    end
+    it "returns error message with invalid request data" do
+      post checkin_path, params: bad_rental_data, as: :json
+
+      body = check_response(expected_type: Hash, expected_status: :not_found)
+      expect(body).must_include "errors"
+      expect(body["errors"]).must_include "rental"
     end
   end
 end
