7f7441302c13e604166041b1b7a237555320b292
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-09 05:32:55 -0800

    Test for RentalsController#checkout

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  let(:customer_id) {customers(:sontag)}
+  let(:movie_id) {movies(:one)}
+  let(:rental_data) {
+    {
+      "movie": movie_id
+      "customer": customer_id
+    }
+  }
+
+  describe "checkout" do
+    it "creates new rental with valid request data" do
+      expect {
+        post checkout_path(rental_data)
+      }.must_change("Rental.count", +1)
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "id"
+
+      rental = Rental.find(body["id"].to_i)
+      # binding.pry
+      expect(rental.movie_id).must_equal rental_data["movie_id"]
+      expect(rental.customer_id).must_equal rental_data["customer_id"]
+      must_respond_with :success
+    end
+    it "returns error message when movie out of stock" do
+    end
+  end
 end
