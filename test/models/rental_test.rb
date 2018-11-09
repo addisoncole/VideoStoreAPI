53bff4c31769ce9d1f934d3356865cad21aef9ba
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-09 14:15:22 -0800

    Tests for custom methods

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -2,6 +2,10 @@ require "test_helper"
 
 describe Rental do
   let(:rental) { rentals(:rentalone) }
+  let(:rental_data) {
+     { "movie_id": movies(:goonies).id,
+       "customer_id": customers(:sontag).id }
+     }
 
   describe "validations" do
 
@@ -39,6 +43,38 @@ describe Rental do
       rental.movie.must_be_kind_of Movie
     end
 
+  end
+
+  describe "set_rental_dates" do
+    it "sets rental dates for valid rental" do
+      rental = Rental.create(rental_data)
+      rental.set_rental_dates
+
+      now = DateTime.now
+      checkout_date = now.strftime('%Y-%m-%d')
+
+      expect(rental.checkout_date.strftime('%Y-%m-%d')).must_equal checkout_date
+
+      due = DateTime.now + 7
+      due_date = due.strftime('%Y-%m-%d')
+
+      expect(rental.due_date.strftime('%Y-%m-%d')).must_equal due_date
+    end
+  end
+
+  describe "check_movie_availability?" do
+    it "returns true if movie is available" do
+      rental = Rental.new(rental_data)
+      expect(rental.check_movie_availability?).must_equal true
+    end
+    it "returns false if movie is not available" do
+      no_inventory_rental = Rental.create(
+        {"movie_id": movies(:no_inventory).id,
+        "customer_id": customers(:sontag).id}
+      )
+
+      expect(no_inventory_rental.check_movie_availability?).must_equal false
+    end
   end
 
 end
